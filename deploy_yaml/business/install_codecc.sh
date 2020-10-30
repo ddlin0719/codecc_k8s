fef298d21f9c4b71b90d21919e72dd369f6e618c
linjun-ddddd <654548718@qq.com>
2020-10-30 20:49:14 +0800

    add codecc docker deploy code

---
+++ deploy_yaml/business/install_codecc.sh
@@ -0,0 +1,20 @@
+# 使用helm安装
+source ../../env.properties
+
+kubectl create ns bkcodecc
+
+if $initConfig; then
+  echo 'unintall bk codecc with initConfig'
+  helm3 uninstall bkcodecc -n bkcodecc
+
+  echo 'install bk codecc with initConfig'
+  helm3 install bkcodecc ./bkcodecc --set image.hub=$hub,volume.nfs.server=$nfs_server --namespace $namespace -f values.yaml
+else
+  echo 'unintall bk codecc'
+  helm3 uninstall bkcodecc -n bkcodecc
+
+  echo 'install bk codecc'
+  helm3 install bkcodecc ./bkcodecc --set image.hub=$hub,volume.nfs.server=$nfs_server --no-hooks --namespace $namespace -f values.yaml
+fi
+
+kubectl get pod -n 'bkcodecc'
