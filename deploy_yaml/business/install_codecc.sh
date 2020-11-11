420ddd75734ae5feebdfa7cb86064a2a454d4a6c
linjun-ddddd <654548718@qq.com>
2020-11-11 20:09:18 +0800

    add pv pvc to async / schedule

--- deploy_yaml/business/install_codecc.sh
+++ deploy_yaml/business/install_codecc.sh
@@ -6,15 +6,15 @@ source codecc_k8s/env.properties
 kubectl create ns bkcodecc
 
 if $initConfig; then
-  echo 'unintall bk codecc with initConfig'
-  helm3 uninstall bkcodecc -n bkcodecc
+  # echo 'unintall bk codecc with initConfig'
+  # helm3 uninstall bkcodecc -n bkcodecc
 
   echo 'install bk codecc with initConfig'
   helm3 install bkcodecc codecc_k8s/deploy_yaml/business/bkcodecc --set image.hub=$hub,volume.nfs.server=$nfs_server \
   --namespace $namespace -f codecc_k8s/deploy_yaml/business/codecc_values.yaml
 else
-  echo 'unintall bk codecc'
-  helm3 uninstall bkcodecc -n bkcodecc
+  # echo 'unintall bk codecc'
+  # helm3 uninstall bkcodecc -n bkcodecc
 
   echo 'install bk codecc'
   helm3 install bkcodecc codecc_k8s/deploy_yaml/business/bkcodecc --set image.hub=$hub,volume.nfs.server=$nfs_server \
